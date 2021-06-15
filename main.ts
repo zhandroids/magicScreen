72c46b9d221b35f4b331abdc992103ff33763c6c
zhandroids <862340288@qq.com>
2021-06-15 11:49:22 +0800

    更新 pxt.json, main.ts, _locales/zh-CN/README.md

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 namespace screenMagic{
     /**
-     * blockId = screenMagicPlotAt
-     * block = "ploat at index"
-     * index.min = 0 index.max = 25
+     *AAAa
      */
+    //% blockId = screenMagicPlotAt
+    //% block = "ploat at index"
+    //% index.min = 0 index.max = 25
     export function plotAt(index:number):void{
         const x = Math.floor(index / 5)
         const y = Math.floor(index % 5)
